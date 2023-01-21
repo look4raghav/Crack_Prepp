83d7b27ec0e9bdefbac46002500c67bf830e07a6
RAGHAV AGGARWAL <look4raghavworld@gmail.com>
2023-01-22 03:10:54 +0530

    22-01

--- counterMiddleLL.c
+++ counterMiddleLL.c
@@ -7,7 +7,7 @@ typedef struct node
     int value;
     struct node *next;
     struct node *prev;
-}mynode;
+}mynode
 
 void add_node(struct node **head, int value);
 void print_list(char *listName, struct node *head);
@@ -15,7 +15,7 @@ void getTheMiddle(mynode *head);
 
 int main() 
 {
-    mynode *head;
+    mynode *head, *middle;
     head = (struct node *)NULL;
 
     add_node(&head, 1);
@@ -29,36 +29,36 @@ int main()
     add_node(&head, 55);
 
     print_list("myList", head);
-    getTheMiddle(head);
+    middle = getTheMiddle(head);
+    printf("\nMiddle Node -> [%d]\n\n", middle->value);
 
     getch();
     return 0;
 }
 
-void getTheMiddle(mynode *head)
+mynode *getTheMiddle(node *head)
 {
-    mynode *p = head;
-    mynode *q = head;
+    mynode *middle = (mynode *)NULL;
+    int i;
 
-    if(q!=NULL)
+    for(i=1; head!=(mynode *)NULL; head=head->next, i++)
     {
-        while((q->next)!=NULL && (q->next->next)!=NULL)
-        {
-            p=(p!=(mynode *)NULL?p->next:(mynode *)NULL);
-            q=(q!=(mynode *)NULL?q->next:(mynode *)NULL);
-            q=(q!=(mynode *)NULL?q->next:(mynode *)NULL); 
-        }
-        printf("The middle element is [%d]", p->value);
+        if(i==1)
+            middle=head;
+        else if((i%2)==1)
+            middle=middle->next;
     }
+    return middle;
 }
 
-void add_node(struct node **head, int value)
+void add_node(struct node, **head, int value)
 {
- mynode *temp, *cur;
- temp = (mynode *)malloc(sizeof(mynode));
- temp->next=NULL;
- temp->prev=NULL;
- if(*head == NULL)
+    mynode *temp, *cur;
+    temp = (mynode *)malloc(sizeof(mynode));
+    temp->next=NULL;
+    temp->prev=NULL;
+
+    if(*head == NULL)
  {
  *head=temp;
  temp->value=value;
@@ -81,4 +81,5 @@ void print_list(char *listName, struct node *head)
  }
 
  printf("NULL\n");
-} 
+}
+}
