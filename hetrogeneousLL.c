2cf7be077b206a980a5b9c0f0b54fe9d142dc0cd
RAGHAV AGGARWAL <look4raghavworld@gmail.com>
2023-01-22 13:04:20 +0530

    22-01

---
+++ hetrogeneousLL.c
@@ -0,0 +1,9 @@
+
+/*
+
+If you are using C language to implement the heterogeneous linked list, what pointer type will you
+use?
+The heterogeneous linked list contains different data types in its nodes and we need a link, pointer to connect them. It is not possible to use
+ordinary pointers for this. So we go for void pointer. Void pointer is capable of storing pointer to any type as it is a generic pointer type. 
+
+*/
