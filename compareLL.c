fc70a4abc6682427a951796efd142252cdab8b79
RAGHAV AGGARWAL <look4raghavworld@gmail.com>
2023-01-29 03:21:43 +0530

    28-01

---
+++ compareLL.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare_linked_lists(struct node *q, struct node *r)
+{
+    static int flag;
+    if((q==NULL)&&(r==NULL))
+    {
+        flag=1;
+    }
+    else
+    {
+        if(q=NULL || r==NULL)
+        {
+            flag=0;
+        }
+        if(q->data!=r->data)
+        {
+            flag=0;
+        }
+        else
+        {
+            compare_linked_lists(q->link,r->link)
+        }
+    }
+    return (flag);
+}
