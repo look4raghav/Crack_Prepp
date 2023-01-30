da6b10d425a1fa62a698d2148e79fda7ec919c18
RAGHAV AGGARWAL <look4raghavworld@gmail.com>
2023-01-30 20:56:47 +0530

    30-01

---
+++ searchLL.c
@@ -0,0 +1,7 @@
+/*
+
+How can I search for data in a linked list?
+The only way to search a linked list is with a linear search, because the only way a linked list?s members can be accessed is sequentially.
+Sometimes it is quicker to take the data from a linked list and store it in a different data structure so that searches can be more efficient.
+
+*/
