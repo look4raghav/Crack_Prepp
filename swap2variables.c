759c7ee990c1b1dd789f531e696cd5296d8536e1
RAGHAV AGGARWAL <look4raghavworld@gmail.com>
2023-02-05 18:05:25 +0530

    05-02

---
+++ swap2variables.c
@@ -0,0 +1,13 @@
+/*
+The best way to swap two variables is to use a temporary variable.
+*/
+
+int a,b,t;
+t = a;
+a = b;
+b = t;
+
+/*
+There is no way better than this as you will find out soon. There are a few slick expressions that do swap variables without using
+temporary storage. But they come with their own set of problems. 
+*/
