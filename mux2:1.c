39324422d9564857edd2270fb52ecf2856324638
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2023-04-23 01:49:13 +0530

    Create mux2:1.c

---
+++ mux2:1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int a; int b; int s;
+  
+  if(s==1)
+  {
+    print("input_1=%d", a);
+  }
+  else if(s==0)
+  {
+    print("input_2=%d", b);
+  }
+  return 0;
+}
